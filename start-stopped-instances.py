e332d916ce83fdc30e4ce605dbd996cb4f3167a2
augustkang <iamaugustkang@gmail.com>
2020-07-10 12:36:28 +0900

    fix to search values from tag dictionary, not keys

--- start-stopped-instances.py
+++ start-stopped-instances.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-# If instance have "target_tag" and in stopped state
-# start instance
+# If an instance's tag have value "target_value" and in stopped state
+# start that instance
 
 import boto3
 
@@ -10,6 +10,6 @@ for instance in ec2.instances.all():
   tmp_dict = {}
   for each_tag in instance.tags:
     tmp_dict[each_tag['Key']] = each_tag['Value']
-  if 'target_tag' in tmp_dict and instance.state['Name'] == 'stopped':
-    print("instance name : " + tmp_dict['Name'] + " instance-id : " + instance.instance_id)
-
+  if 'target_value' in tmp_dict.values() and instance.state['Name'] == 'stopped':
+    instance.start()
+    print("instance " + tmp_dict['Name'] +" started")
